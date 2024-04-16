3b904c3457d1b6104b022c7c99799d8477ca2e8b
harelog3 <harel.olguin@iteso.mx>
2024-04-16 11:37:45 -0600

    gitsigns config

--- lua/harel/plugins/gitsigns.lua
+++ lua/harel/plugins/gitsigns.lua
@@ -1,5 +1,14 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = true,
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    current_line_blame = true,
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+      delay = 250,
+      ignore_whitespace = false,
+      virt_text_priority = 100,
+    },
+  },
 }
